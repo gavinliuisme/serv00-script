ad14fadc00c70f429712174b1ce4cd8ccf362570
星峰 <34774269+gavinliuisme@users.noreply.github.com>
2024-07-29 17:17:58 +0800

    Update keeponline.sh

--- vless/keeponline.sh
+++ vless/keeponline.sh
@@ -1,4 +1,5 @@
 pm2_path=$(which /home/$USER/.npm-global/bin/pm2)
+
 process_count=$($pm2_path list | grep "vless" | wc -l)
 if [ "$process_count" -gt 1 ]; then
     process_list=$($pm2_path  jlist | jq -r --arg name "vless" '.[] | select(.name == $name) | .pm_id')
@@ -11,8 +12,20 @@ if [ "$process_count" -gt 1 ]; then
 fi
 status=$($pm2_path  info vless | grep "status" | awk '{print $4}')
 if [ "$status" == "" ]; then
-    $pm2_path  resurrect
-    echo "还原vless进程"
+    if [[ -f /home/$USER/.pm2/dump.pm2 ]]; then
+        $pm2_path  resurrect
+        echo "还原vless进程"
+    else
+        if [[ -f /home/$USER/domains/$USER.serv00.net/vless/app.js ]]; then
+            $pm2_path start /home/$USER/domains/$USER.serv00.net/vless/app.js --name vless
+            $pm2_path save
+        fi
+        if [[ -f /home/$USER/domains/$USER.ct8.pl/vless/app.js ]]; then
+            $pm2_path start /home/$USER/domains/$USER.ct8.pl/vless/app.js --name vless
+            $pm2_path save
+        fi
+        echo "未检测到pm2 vless快照，启动vless进程...,并保存快照"
+    fi
 elif [ "$status" != "online" ]; then
     $pm2_path  restart vless
     echo "重启vless进程"
